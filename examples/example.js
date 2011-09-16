ccab1910778b83d9763509575a02afea3274000f
Vadim M. Baryshev <vadimbaryshev@gmail.com>
2011-09-16 23:32:31 +0400

    First commit

---
+++ examples/example.js
@@ -0,0 +1,50 @@
+var aop = require("./../lib/aop");
+
+var cache = {};
+
+var someAsyncFunction = function fnName(num, callback) {
+	console.log("calculating");
+	var result = num * 2;
+	callback(null, result);
+	cache[num] = result;
+};
+
+var cacheAspect = function(method, params) {
+	console.log(method.name);
+	var num = params[0];
+	if (cache[num]) {
+		var callback = params[params.length - 1];
+		console.log("using cached value");
+		callback(null, cache[num]);
+	} else {
+		method.apply(this, params);
+	}
+};
+
+var changeAspect = function(method, params) {
+	console.log(this.name);
+	params[0]++;
+	method.apply(this, params);
+};
+
+var formatAspect = function(method, params) {
+	console.log(method.name);
+	var callback = params[params.length - 1];
+	var newCallback = function(error, result) {
+		callback(error, result + "s");
+	};
+	params[params.length - 1] = newCallback;
+	method.apply(this, params);
+};
+
+someAsyncFunction = aop.wrap(someAsyncFunction, someAsyncFunction, cacheAspect);
+someAsyncFunction = aop.wrap(someAsyncFunction, someAsyncFunction, formatAspect);
+someAsyncFunction = aop.wrap(someAsyncFunction, someAsyncFunction, changeAspect);
+
+someAsyncFunction(2, function(error, result) {
+	console.log(result);
+});
+
+someAsyncFunction(2, function(error, result) {
+	console.log(result);
+});
